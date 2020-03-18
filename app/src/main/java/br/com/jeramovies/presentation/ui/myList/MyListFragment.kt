40c9f18b3a1050a861bf3c7ccc9697af01d8ebec
Luiz Filho <luiz@jera.com.br>
2020-03-18 17:16:00 -0400

    Adding go to

--- app/src/main/java/br/com/jeramovies/presentation/ui/myList/MyListFragment.kt
+++ app/src/main/java/br/com/jeramovies/presentation/ui/myList/MyListFragment.kt
@@ -6,7 +6,9 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import br.com.jeramovies.databinding.FragmentMyListBinding
+import br.com.jeramovies.presentation.ui.main.MainViewModel
 import br.com.jeramovies.presentation.util.livedata.observe
+import org.koin.android.viewmodel.ext.android.sharedViewModel
 import org.koin.android.viewmodel.ext.android.viewModel
 
 class MyListFragment : Fragment() {
@@ -14,6 +16,7 @@ class MyListFragment : Fragment() {
     private lateinit var binding: FragmentMyListBinding
     private val myListMovieAdapter by lazy { MyListMovieAdapter(viewModel::onMovieClicked) }
     private val viewModel: MyListViewModel by viewModel()
+    private val activityViewModel: MainViewModel by sharedViewModel()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -29,6 +32,7 @@ class MyListFragment : Fragment() {
     private fun subscribeUi() {
         //TODO -> Get movies from Realm or Room
         viewModel.savedMovies.observe(this, myListMovieAdapter::submitList)
+        viewModel.goTo.observe(this, activityViewModel::goTo)
     }
 
     private fun setupRecyclerView() {
